019b3eac158b727b18778ff0f285ac994035004e
박 상 민 <dhwkffks7@naver.com>
2022-07-10 14:17:13 +0900

    Adapter와 activity의 viewPager 어댑터 연결

--- app/src/main/java/com/sangmin/viewpagerpractice_20220710/MainActivity.kt
+++ app/src/main/java/com/sangmin/viewpagerpractice_20220710/MainActivity.kt
@@ -2,10 +2,20 @@ package com.sangmin.viewpagerpractice_20220710
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.sangmin.viewpagerpractice_20220710.adapters.MainViewPagerAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mPagerAdapter : MainViewPagerAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mPagerAdapter = MainViewPagerAdapter(supportFragmentManager)
+        mainViewPager.adapter = mPagerAdapter
+
+
     }
 }
