5c89b97bd1d34f8c5810bb5276a84441090cf244
박 상 민 <dhwkffks7@naver.com>
2022-07-10 14:13:30 +0900

    Adapter 제작 완료

---
+++ app/src/main/java/com/sangmin/viewpagerpractice_20220710/adapters/MainViewPagerAdapter.kt
@@ -0,0 +1,31 @@
+package com.sangmin.viewpagerpractice_20220710.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.sangmin.viewpagerpractice_20220710.fragments.BirthFragment
+import com.sangmin.viewpagerpractice_20220710.fragments.HelloFragment
+import com.sangmin.viewpagerpractice_20220710.fragments.NameFragment
+
+class MainViewPagerAdapter(fm:FragmentManager) : FragmentPagerAdapter(fm) {
+//    총 몇 페이지의 뷰페이저인지 숫자를 알려달라
+    override fun getCount(): Int {
+        return 3
+
+    }
+
+//   각각의 position에 어떤 프래그먼트 객체가 결과로 return될건지 알려달라.
+    override fun getItem(position: Int): Fragment {
+        when(position) {
+            0 -> {
+                return NameFragment()
+            }
+            1 -> {
+                return BirthFragment()
+            }
+            else -> {
+                return HelloFragment()
+            }
+        }
+    }
+}
