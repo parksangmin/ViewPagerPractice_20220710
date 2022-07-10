7e264054f5e7e2fe4fecaa7c3be789367f51a37c
박 상 민 <dhwkffks7@naver.com>
2022-07-10 13:40:35 +0900

    Initial commit

---
+++ app/src/main/java/com/sangmin/viewpagerpractice_20220710/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.sangmin.viewpagerpractice_20220710
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
